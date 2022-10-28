5116ff11a5adfed725712eb9d7398ce77bae4ad6
Daniel De Guzman <50926547+DenDanDin@users.noreply.github.com>
2022-10-27 23:37:10 -0700

    Revert "1:19:40 - UI done"
    This reverts commit 908cad9f4466321139a3232b99ee93782df9650e.

--- react-task-tracker/src/components/Task.js
+++ react-task-tracker/src/components/Task.js
@@ -1,14 +1,12 @@
 import { FaTimes } from 'react-icons/fa'
 
-const Task = ({ task, onDelete, onToggle }) => {
+const Task = ({ task, onDelete }) => {
   return (
-    <div className={`task ${task.reminder ? 'reminder' : ''}`} onDoubleClick={() => onToggle(task.id)}>
+    <div className='task'>
         <h3>
-            {task.text}{' '} 
-            <FaTimes 
-            style={{color: 'red', cursor: 'pointer'}}
-            onClick={() => onDelete(task.id)}
-            />
+            {task.text} <FaTimes style={{color: 
+            'red', cursor: 'pointer'}}
+            onClick={() => onDelete(task.id)}/>
         </h3>
         <p>{task.day}</p>
 
